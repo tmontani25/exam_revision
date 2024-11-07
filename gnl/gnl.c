ac6225a93e64fdc22b085990f3ceb04b8fc32dc9
tmontani25 <teomontani27@gmail.com>
2024-11-07 10:58:15 +0100

    gnl still

--- gnl/gnl.c
+++ gnl/gnl.c
@@ -1,17 +1,42 @@
 #include "gnl_test.h"
 
+
+// garder les bytes lu dans une chaine
+//verifier /n
+//join les caracteres suivant
+//
+
+static char *ft_read(int fd, char *buf)
+{
+    int bytes_read;
+    char *line;
+    char *backup;
+    
+
+    bytes_read = read(fd, buf, BUFFER_SIZE);
+    if (bytes_read == -1)
+    {
+        free(buf);
+        return (NULL);
+    }
+    while (bytes_read > 0)
+    {
+        if (!backup)
+            backup = strdup(buf);
+        else if (backup)
+
+        
+    }
+}
 char *get_next_line(int fd)
 {
     char *buf;
     char *line;
+    
+    buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    line = ft_read(fd, buf);
 
-    while (1)
-    {
-        buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-        if (!read(fd, buf, BUFFER_SIZE))
-            puts("read error\n");
-        line = strcat(line, buf);
-        free(buf);
+
 
 
 }
@@ -25,4 +50,5 @@ int main(void)
     path = "fichier.txt";
     fd = open(path, O_RDONLY);
     result = get_next_line(fd);
+    printf("line: %s\n", result)
 }
